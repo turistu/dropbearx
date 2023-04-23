03a9e87431792afaddb97850b63cb5aaf4029ea9
turistu <128076160+turistu@users.noreply.github.com>
2023-04-24 02:08:40 +0300

    dropbear_exit: show error message

--- src/svr-chansession.c
+++ src/svr-chansession.c
@@ -1081,7 +1081,7 @@ void svr_chansessinitialise() {
 	sa_chld.sa_flags = SA_NOCLDSTOP;
 	sigemptyset(&sa_chld.sa_mask);
 	if (sigaction(SIGCHLD, &sa_chld, NULL) < 0) {
-		dropbear_exit("signal() error");
+		dropbear_exit("sigaction:");
 	}
 	
 }
