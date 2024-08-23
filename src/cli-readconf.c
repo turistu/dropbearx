37f5e6661e0274e1277eff4238a458f9d9c01f29
Matt Johnston <matt@ucc.asn.au>
2024-08-23 22:41:31 +0800

    Fix signedness complaint in cli-readconf

--- src/cli-readconf.c
+++ src/cli-readconf.c
@@ -69,7 +69,8 @@ void read_config_file(char* filename, FILE* config_file, cli_runopts* options) {
 	while (buf_getline(buf, config_file) == DROPBEAR_SUCCESS) {
 		char* commentStart = NULL;
 		cfg_option cfg_opt;
-		int found, i;
+		int found;
+		size_t i;
 		/* Update line number counter. */
 		linenum++;
 
